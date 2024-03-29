e0c8047acb54f3be22f2cef1d8c9d80c87127600
Quaaludees <quaalud@gmail.com>
2024-03-29 13:21:06 +0300

    add components header and search

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,5 +1,7 @@
-import { AbstractView } from "../../common/view.js";
+import {AbstractView} from "../../common/view.js";
 import onChange from "on-change";
+import {Header} from "../../components/header/header.js";
+import {Search} from "../../components/search/search.js";
 
 export class MainView extends AbstractView {
     state = {
@@ -8,23 +10,31 @@ export class MainView extends AbstractView {
         searchQuery: undefined,
         offset: 0
     };
+
     constructor(appState) {
         super();
         this.appState = appState;
         this.appState = onChange(this.appState, this.appStateHook.bind(this));
         this.setTitle('Поиск книг');
     }
-    appStateHook (path) {
+
+    appStateHook(path) {
         console.log(path);
         if (path === 'favorites') {
             console.log(path);
         }
     }
+
     render() {
         const main = document.createElement('div');
-        main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
+        main.append(new Search(this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
-        this.appState.favorites.push('d');
+        this.renderHeader();
+    }
+
+    renderHeader() {
+        const header = new Header(this.appState).render();
+        this.app.prepend(header);
     }
 }
